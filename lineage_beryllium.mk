096e6ba94f6a840fc759079e34984a49196ed4b4
Shikhar <krtonia@gmail.com>
2022-11-14 22:05:33 +0530

    beryllium:riceDroid
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -9,6 +9,31 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# riceDroid Stuff with GApps
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+SUSHI_BOOTANIMATION  := 1080
+TARGET_ENABLE_BLUR := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Gapps
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+
+# Package Type (By default it says AOSP,lets make it Vanilla instead)
+RICE_PACKAGE_TYPE := Vanilla
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# AudioFx
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Maintainer Stuff
+RICE_MAINTAINER := Shikhar
+RICE_OFFICIAL := false
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
